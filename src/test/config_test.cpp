bd2d393fc2fc48186aa9ebea178445e89ed85fd4
Ethan Moffat <ethan@moffat.io>
2021-09-10 17:41:22 -0700

    Fix setenv usage in windows

--- src/test/config_test.cpp
+++ src/test/config_test.cpp
@@ -1,12 +1,17 @@
 #include <gtest/gtest.h>
+
 #include "config.hpp"
 #include "console.hpp"
 #include "eoserv_config.hpp"
 
+#ifdef _WIN32
+#include "eoserv_windows.h"
+#endif
+
 GTEST_TEST(ConfigTests, EoservConfig_ValueFromEnvironmentVariable_OverridesFileValue)
 {
     Console::SuppressOutput(true);
-    ::setenv("ETHEOS_PORT", "12345", 1);
+    setenv("ETHEOS_PORT", "12345", 1);
 
     Config config;
     eoserv_config_validate_config(config);
