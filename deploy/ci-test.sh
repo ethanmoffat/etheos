462d370ef0ba0cf2e9d548da8584eb9940f7f94b
Ethan Moffat <ethan@moffat.io>
2022-03-04 00:24:40 -0800

    Add integration test to ci test script

--- deploy/ci-test.sh
+++ deploy/ci-test.sh
@@ -65,6 +65,7 @@ function main() {
   ${botdir}/EOBot host=${host} port=${port} bots=6 script=${SCRIPT_ROOT}/../src/test/integration/change_passwords.eob -- "BotP@ssw0rd" "NewPassword"
   ${botdir}/EOBot host=${host} port=${port} bots=6 script=${SCRIPT_ROOT}/../src/test/integration/change_passwords.eob -- "NewPassword" "BotP@ssw0rd"
   ${botdir}/EOBot host=${host} port=${port} bots=6 script=${SCRIPT_ROOT}/../src/test/integration/login_queue_busy.eob
+  ${botdir}/EOBot host=${host} port=${port} bots=6 script=${SCRIPT_ROOT}/../src/test/integration/create_delete_char.eob
 
   return 0
 }
