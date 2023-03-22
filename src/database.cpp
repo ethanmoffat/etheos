e893e457b00b3481e448628a0b93ad763f09415e
Ethan Moffat <ethan@moffat.io>
2023-03-22 10:33:37 -0700

    Update sqlite3 library version mismatch text to be more consistent/clear

--- src/database.cpp
+++ src/database.cpp
@@ -389,15 +389,15 @@ void Database::Connect(Database::Engine type, const std::string& host, unsigned 
 				if (sqlite3_libversion_number() > SQLITE_VERSION_NUMBER)
 				{
 					Console::Wrn("SQLite library runtime version is greater than the library version that etheos was compiled with.");
-					Console::Wrn("  Installed version: %s", sqlite3_libversion());
-					Console::Wrn("  etheos version:    %s", SQLITE_VERSION);
-					Console::Wrn("This could cause crashes with SQLite calls.");
+					Console::Wrn("  Compiled version: %s", SQLITE_VERSION);
+					Console::Wrn("  Runtime version:  %s", sqlite3_libversion());
+					Console::Wrn("This could cause issues if you have modified etheos to use non-stable sqlite3 API calls.");
 				}
 				else
 				{
-					Console::Err("SQLite library version mismatch! Please recompile EOSERV with the correct SQLite library.");
-					Console::Err("  Expected version: %s", SQLITE_VERSION);
-					Console::Err("  Library version:  %s", sqlite3_libversion());
+					Console::Err("SQLite library runtime version is incompatible with the library version that etheos was compiled with.");
+					Console::Err("  Compiled version: %s", SQLITE_VERSION);
+					Console::Err("  Runtime version:  %s", sqlite3_libversion());
 #ifdef WIN32
 					Console::Err("Make sure EOSERV is using the correct version of sqlite3.dll");
 #endif // WIN32
