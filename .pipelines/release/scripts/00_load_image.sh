88a98cfcf3d1144d6841d24e36b1eff3beb240ce
Ethan Moffat <ethan@moffat.io>
2025-02-02 21:12:56 -0800

    Add publish steps to CI workflow; delete old pipeline definition

--- .pipelines/release/scripts/00_load_image.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-gzip -f -d $1/image.tar.gz
-docker load -i $1/image.tar
--- .pipelines/release/scripts/02_get_data.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-wget "$1" 2>&1 > /dev/null
-unzip -o sample-data.zip -d "$2/.."
